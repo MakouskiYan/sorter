1803dd9d9a1fc533e31d0dc03155f71c34c03ea1
Yan Makouski <makovskiy.yan@gmail.com>
2018-09-17 12:06:55 +0300

    wrote the solution with some comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.length = 0;
+    this.compare = null;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
+    this.length+=1;
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
+  }
+  //in es6 i need "_" bcs if i do not use undersrcore it will  
+  //calls setter(getter) again and again, wich leads to an infinite recursion.
+  get length() {
+    return this._length;
   }
 
-  get length() {
-    // your implementation
+  set length(value) {
+    this._length = value; 
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
-
+  // if we do not have a template how to sort (in setComparator)
+  // it will sort just in regular mode (in ascending style)
   sort(indices) {
-    // your implementation
+    var ar = [];
+    for(var i = 0; i<indices.length;i++){
+      ar.push(this.array[indices[i]]);
+    }
+    //we need to sort indices too in ascending mode,
+    //otherwise it will not change our array correct.
+    indices = indices.sort((a,b)=>a-b);
+    // before we do not have any template our compare function is equal null
+    // and before sorting we check if we have a template for sorting
+    if(this.compare === null){
+      ar = ar.sort((a,b)=>a-b);
+    }else{
+      ar = ar.sort(this.compare);
+    }
+    //after sorting we need to place our elements in indices we choose to sort originally
+    for(var i=0;i<ar.length;i++){
+      this.array[indices[i]] = ar[i];
+    }
   }
-
+  // we initial our compare function
   setComparator(compareFunction) {
-    // your implementation
+    this.compare = compareFunction;
   }
 }
 
